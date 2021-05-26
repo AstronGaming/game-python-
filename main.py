fd8b8d38bfbc0c1986b36c159754b67b4a940524
Astron Gaming <yashudkl@gmail.com>
2021-05-26 08:36:02 +0545

    Most of the things are commneted

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import random
-import time
-def game(comp , you): #made a functuon to choose winner or returns who wins
+import random #random module imported
+import time # time module imported
+def game(comp , you): #made a functuon to choose winner // returns who wins
     if comp == you:
         return None
-    if comp == 's':
+    if comp == 's': #if computer chooses s 
         if you == 'g':
             return True
         elif you == 'w':
             return False
-    if comp == 'g':
+    if comp == 'g': #if computer chooses g
         if you == 's':
             return False
         elif you == 'w':
             return True
-    if comp == 'w':
+    if comp == 'w': #if computer chooses w
         if you == 's':
             return True
         elif you == 'g':
@@ -42,7 +42,7 @@ elif rand == 2:
     comp = 'g'
 elif rand == 3:
     comp = 'w'
-time.sleep(0.5)
+time.sleep(0.5) #for realiastic look freezed for 0.5 sec
 print(f'''
 Computer choosed {comp} and you choosed {you}''')
 
